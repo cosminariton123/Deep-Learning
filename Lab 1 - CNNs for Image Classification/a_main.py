502e32ba1a219d3f53482e38d6a4494cc3c5962f
Darkrider123 <cosmin.ariton@gmail.com>
2022-10-18 01:46:34 +0300

    refactoring

---
+++ Lab 1 - CNNs for Image Classification/a_main.py
@@ -0,0 +1,19 @@
+import b_tensor_operations
+import c_classification_on_the_cifar_dataset
+import d_smallnorb_dataset_cnn
+import e_model_evaluation
+import f_saving_models
+import g_loading_models
+
+
+def main():
+    #b_tensor_operations.main()
+    #c_classification_on_the_cifar_dataset.main()
+    #d_smallnorb_dataset_cnn.main()
+    #e_model_evaluation.main()
+    #f_saving_models.main()
+    g_loading_models.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Lab 1 - CNNs for Image Classification/g_loading_models.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def load_best_model(): 
+    return tf.keras.models.load_model('./checkpoints/best_model.hdf5')
+
+
+def predict_1_random(model):
+    ds, meta = tfds.load('cifar10', as_supervised = True, with_info = True)
+    test_ds = ds["test"]
+    ds_as_numpy = tfds.as_numpy(test_ds)
+
+    random_image, label = random.choice(list(ds_as_numpy))
+    plt.title(label)
+    plt.imshow(random_image)
+    plt.show()
+
+    im = np.float32(
+    np.reshape(
+                random_image,
+                [1, 32, 32, 3],
+                )
+            )
+    print("Prediction: ", np.argmax(model.predict(im), axis = -1), " Ground truth: ", label)
+
+
+def predict_test_dataset(model):
+    ds, meta = tfds.load('cifar10', as_supervised = True, with_info = True)
+    test_ds = ds['test']
+    model.evaluate(test_ds.batch(64))
+
+def main():
+    model = load_best_model()
+    predict_1_random(model)
+    predict_test_dataset(model)
+
+if __name__ == '__main__':
+    main()

5cbf28c0ae0cbb10497f469ac586546a23f40c5c
Darkrider123 <cosmin.ariton@gmail.com>
2022-10-19 17:19:45 +0300

    added custom metric example

---
+++ Lab 1 - CNNs for Image Classification/k_defining_custom_metrics.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+keras = tf.keras
+import keras.layers as layers
+import math
+import os
+import shutil
+
+
+def make_model(input_shape, custom_metric):
+    input = tf.keras.Input(shape = input_shape)
+    layer = input
+
+    for _ in range(2):
+        layer = layers.Conv2D(64, (5, 5), activation = "relu")(layer)
+        layer = layers.MaxPool2D((2, 2))(layer)
+    layer = layers.Flatten()(layer)
+    for u, a in zip([64, 10], ["relu", "softmax"]):
+        layer = layers.Dense(u, activation = a)(layer)
+
+    model = tf.keras.Model(inputs = input, outputs = layer)
+    model.compile(
+        optimizer = "adam",
+        loss = "sparse_categorical_crossentropy",
+        metrics = [custom_metric]
+    )
+    
+    return model
+
+
+def train_with_custom_metrics(custom_metric):
+    batch_size = 64
+    model = make_model((28, 28, 1), custom_metric)
+    (train_ds, val_ds), meta = tfds.load(
+                                        'mnist',
+                                        as_supervised = True,
+                                        with_info = True,
+                                        split = [ 'train[:80%]', 'train[20%:]'],
+                                        shuffle_files = True
+                                        )
+    n_samples = meta.splits['train'].num_examples
+
+    test_ds, meta = tfds.load(
+        "mnist",
+        as_supervised = True,
+        with_info = True,
+    )
+    test_ds = test_ds["test"]
+
+    model.fit(
+    train_ds.batch(batch_size).repeat(),
+    steps_per_epoch = math.ceil(n_samples * 0.8 / batch_size),
+    epochs = 5,
+    validation_data = val_ds.batch(batch_size),
+    validation_steps = math.ceil(n_samples * 0.2 / batch_size),
+    shuffle = False,
+    )
+    print(model.evaluate(test_ds.batch(batch_size)))
+
+
+def custom_metric(y_true, y_pred):
+    return tf.reduce_mean(tf.reduce_max(y_pred, axis = -1))
+
+
+
+def main():
+    train_with_custom_metrics(custom_metric)
+
+
+if __name__ == "__main__":
+    main()
+

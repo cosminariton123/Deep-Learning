63d2bb3a5266c63b2d1d215e766887a76b97f306
Darkrider123 <cosmin.ariton@gmail.com>
2022-10-31 18:14:37 +0200

    Called main functions from all implemented files

--- Lab 1 - CNNs for Image Classification/a_main.py
+++ Lab 1 - CNNs for Image Classification/a_main.py
@@ -4,15 +4,23 @@ import d_smallnorb_dataset_cnn
 import e_model_evaluation
 import f_saving_models
 import g_loading_models
+import h_loading_custom_data
+import i_preprocessing
+import j_tensorboard_logs
+import k_defining_custom_metrics
 
 
 def main():
-    #b_tensor_operations.main()
-    #c_classification_on_the_cifar_dataset.main()
-    #d_smallnorb_dataset_cnn.main()
-    #e_model_evaluation.main()
-    #f_saving_models.main()
+    b_tensor_operations.main()
+    c_classification_on_the_cifar_dataset.main()
+    d_smallnorb_dataset_cnn.main()
+    e_model_evaluation.main()
+    f_saving_models.main()
     g_loading_models.main()
+    h_loading_custom_data.main()
+    i_preprocessing.main()
+    j_tensorboard_logs.main()
+    k_defining_custom_metrics.main()
 
 
 if __name__ == '__main__':

f1d7ac2d894f51407614d4ed6e79ebf0bfd6b8f9
Darkrider123 <cosmin.ariton@gmail.com>
2022-11-08 13:17:09 +0200

    created model

--- Lab 2 - CNNs for Text Classification/Lab2_exercise.py
+++ Lab 2 - CNNs for Text Classification/Lab2_exercise.py
@@ -26,44 +26,18 @@ def get_cnn_model_v1():
 
     model = Sequential()
 
-    # 3.2. Add the layers (check out the work done in the previous lab)
-
-    ########################################################################################
-    # 3.2.1. Add an embedding layer which maps our vocab indices (max_features) into embedding_dims dimensions
     model.add(Embedding(max_features, embedding_dims, input_length=maxlen))
-
-    # TODO 3.2.2. Dropout with a probability of 0.4
     model.add(Dropout(0.4))
-
-    # TODO 3.2.3. Add a Convolution1D layer, with 128 filters, kernel size ks[0], padding same, activation relu and stride 1
-    model.add(Conv1D(filters = filters, kernel_size = ks[0], padding = "same", activation = "relu", strides = 1))
-
-    # TODO 3.2.4. Use max pooling after the CONV layer
+    model.add(Conv1D(128, 3, 1, "same", activation="relu"))
     model.add(MaxPooling1D())
-
-    # TODO 3.2.5. Add a CONV layer, similar in properties to what we have above (3.2.3.) and kernel size 5
-    model.add(Conv2D(filters = filters, kernel_size = ks[1], padding = "same", activation = "relu", strides = 1))
-
-    # TODO 3.2.6. Add a Batch Normalization layer in order to reduce overfitting
+    model.add(Conv1D(128, 5, 1, "same", activation="relu"))
     model.add(BatchNormalization())
-
-    # TODO 3.2.7. Use max pooling again
     model.add(MaxPooling1D())
-
-    # TODO 3.2.8. Add a flatten layer
     model.add(Flatten())
-
-    # TODO 3.2.9. Add a dense layer with hidden_dims hidden units and activation relu
-    model.add(Dense(hidden_dims = hidden_dims, activation = "relu"))
-
-    # TODO 3.2.10. Add a dropout layer with a dropout probability of 0.5 
+    model.add(Dense(128, activation="relu"))
     model.add(Dropout(0.5))
+    model.add(Dense(1, activation="sigmoid"))
 
-    # TODO 3.2.11. We project onto a single unit output layer, and squash it with a sigmoid
-    model.add(Dense(1, activation='sigmoid'))
-    ##################################################################################
-
-    # TODO 3.3. Compile the model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     model.summary()
@@ -84,11 +58,10 @@ def main():
 
 
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000)
+    #x_train = x_train[20:]
 
     print(len(x_train), 'train sequences')
     print(len(x_test), 'test sequences')
-
-    
 
     x_train = keras.utils.pad_sequences(x_train, maxlen = maxlen)
     x_test = keras.utils.pad_sequences(x_test, maxlen = maxlen)
